5758b7a73acbf0dbf6538c771cbf128591756a67
Samantha <9019706@student.zadkine.nl>
2024-03-13 17:51:35 +0100

    db connectie gemaakt

---
+++ dbconnectie.php
@@ -0,0 +1,20 @@
+<?php
+
+class Controller{
+
+    private $host ='localhost';
+    private $user ='root';
+    private $password = '';
+    private $database = 'biebliotheek';
+
+    public $con = null;
+
+    public function __construct()
+    {
+        $this->con = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+        if ($this->con->connect_error) {
+            die("Connection failed: " . $this->con->connect_error);
+        }
+        echo "Connection Successful...!";
+        }
+    }

7ad270382d2ba72464625f1abb7bdc97469c3794
Rubens120 <112856976+Rubens120@users.noreply.github.com>
2024-03-19 11:25:37 +0100

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,17 @@
+<html>
+<header>
+
+</header>
+<main>
+
+<body>
+<section>
+    <label for="email">password</label>
+    <input type="email" placeholder="email">
+    <label for="password">password</label>
+    <input type="password" placeholder="password">
+    <button>login</button>
+</section>
+</body>
+</main>
+</html>
